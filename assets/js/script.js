e43fb9fe71f0c17e1c2ccd1ee0f052d33c2203ef
DustyMick <damicklo5@gmail.com>
2021-01-06 13:42:24 -0600

    add ability to update and delete tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var pageContentEl = document.querySelector("#page-content");
 var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 
 var taskFormHandler = function(event) {
@@ -149,6 +151,25 @@ var createTaskEl = function(taskDataObj) {
     taskSelected.remove();
   };
 
+  var taskStatusChangeHandler = function(event) {
+    var taskId = event.target.getAttribute("data-task-id");
+    var statusValue = event.target.value.toLowerCase();
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    if (statusValue === "to do") {
+      tasksToDoEl.appendChild(taskSelected);
+    } 
+    else if (statusValue === "in progress") {
+      tasksInProgressEl.appendChild(taskSelected);
+    } 
+    else if (statusValue === "completed") {
+      tasksCompletedEl.appendChild(taskSelected);
+    }
+  };
+  
+
+  pageContentEl.addEventListener("change", taskStatusChangeHandler);  
+
   pageContentEl.addEventListener("click", taskButtonHandler);  
 
   formEl.addEventListener("submit", taskFormHandler);
@@ -156,6 +177,8 @@ var createTaskEl = function(taskDataObj) {
     listItemEl.className = "task-item";
     listItemEl.textContent = taskNameInput;
     tasksToDoEl.appendChild(listItemEl);
+
+  
 
   
 
