f42a3ff8158560c33a57527c355d18f3bd778752
DustyMick <damicklo5@gmail.com>
2021-01-06 18:17:35 -0600

    dragleave and drag style added

--- assets/js/script.js
+++ assets/js/script.js
@@ -180,6 +180,7 @@ var createTaskEl = function(taskDataObj) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
       event.preventDefault();
+      taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
   };
 
@@ -199,8 +200,20 @@ var createTaskEl = function(taskDataObj) {
     else if (statusType === "tasks-completed") {
       statusSelectEl.selectedIndex = 2;
     }
+
+    dropZoneEl.removeAttribute("style");
+
     dropZoneEl.appendChild(draggableElement);
   };
+
+  var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+      taskListEl.removeAttribute("style");
+    }
+  }
+
+  pageContentEl.addEventListener("dragleave", dragLeaveHandler);
   
   pageContentEl.addEventListener("drop", dropTaskHandler);
 
